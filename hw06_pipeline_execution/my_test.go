1152f836602a88a87012039e9d50b2fc996e0c1b
AngelCareMe <loktev356@inbox.ru>
2025-07-30 19:00:10 +0300

    HW6 is completed

---
+++ hw06_pipeline_execution/my_test.go
@@ -0,0 +1,45 @@
+package hw06pipelineexecution
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require" //nolint:depguard
+)
+
+func TestEmptyPipeline(t *testing.T) {
+	in := make(Bi)
+	go func() {
+		close(in)
+	}()
+	result := make([]interface{}, 0)
+	for v := range ExecutePipeline(in, nil, []Stage{}...) {
+		result = append(result, v)
+	}
+	require.Empty(t, result, "Empty pipeline should produce no output")
+}
+
+func TestSingleStage(t *testing.T) {
+	in := make(Bi)
+	data := []int{1, 2, 3}
+	go func() {
+		for _, v := range data {
+			in <- v
+		}
+		close(in)
+	}()
+	stage := func(in In) Out {
+		out := make(Bi)
+		go func() {
+			defer close(out)
+			for v := range in {
+				out <- v.(int) + 1
+			}
+		}()
+		return out
+	}
+	result := make([]interface{}, 0)
+	for v := range ExecutePipeline(in, nil, stage) {
+		result = append(result, v)
+	}
+	require.Equal(t, []interface{}{2, 3, 4}, result)
+}
--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,37 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	in = wrapWithDone(in, done)
+	for _, stage := range stages {
+		in = wrapWithDone(stage(in), done)
+	}
+	return in
+}
+
+func wrapWithDone(in In, done In) Out {
+	out := make(chan interface{})
+	go func() {
+		defer close(out)
+		for {
+			select {
+			case <-done:
+				// дочитываем канал, чтобы upstream не заблокировался
+				for range in {
+				}
+				return
+			case val, ok := <-in:
+				if !ok {
+					return
+				}
+				select {
+				case <-done:
+					for range in {
+					}
+					return
+				case out <- val:
+				}
+			}
+		}
+	}()
+	return out
 }
